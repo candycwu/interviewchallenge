d15b6e76d63535301935881c7911dc7d73912e9a
Candy W <lee.candy1990@icloud.com>
2017-09-12 07:57:26 +0000

    authentication is working; registration error

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ var express               = require("express"),
 
 seedDB();
 
+//mongoose database
+mongoose.connect("mongodb://localhost/challenge");
+//to use bodyParser
+app.use(bodyParser .urlencoded({extended: true}));
+//to omit ejs at the end of files
+app.set("view engine", "ejs");
+
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
+    //secret - to encode or decode the session
     secret: "Nami and Jelly are the cutest dogs ever and forever",
     resave: false,
     saveUninitialized: false
@@ -21,31 +29,17 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()));
+//responsible for taking the data in the session encoding and unencoding
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-//mongoose database
-mongoose.connect("mongodb://localhost/challenge");
-
-//to use bodyParser
-app.use(bodyParser .urlencoded({extended: true}));
-
-//to omit ejs at the end of files
-app.set("view engine", "ejs");
-
 
 //root path
 app.get("/", function(req, res){
     res.render("login");
 });
 
-//register path
-app.get("/register", function(req, res){
-    res.render("register");
-});
-
 //form path
-app.get("/form", function(req, res){
+app.get("/form",  isLoggedIn, function(req, res){
             Form.find({}, function(err, allForms){
                 if(err){
                     console.log(err);
@@ -56,13 +50,13 @@ app.get("/form", function(req, res){
 });
 
 //new form path
-app.post("/form", function(req, res){
-    var name = req.body.name;
+app.post("/form",  isLoggedIn, function(req, res){
+    var username = req.body.username;
     var email = req.body.email;
     var message = req.body.message;
-    var newForm = {name:name, email:email, message:message};
+    var newForm = {username:username, email:email, message:message};
     //create new form and save to db
-    Form.create(newForm, function(err, newlyCreated){
+    Form.create(newForm, isLoggedIn, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
@@ -73,7 +67,7 @@ app.post("/form", function(req, res){
 });
 
 //show more info about clicked form
-app.get("/form/:id", function(req, res){
+app.get("/form/:id", isLoggedIn, function(req, res){
     Form.findById(req.params.id, function(err, foundForm){
         if(err){
             console.log(err);
@@ -83,6 +77,62 @@ app.get("/form/:id", function(req, res){
         }
     }
 )});
+
+//=========================
+//  AUTHENTICATION ROUTES
+//=========================
+
+//register path
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+//handling user signup
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+           return res.render("register");
+       } 
+       //this line logs the user in & take care of the session and store info & it'll run the serialized user method
+       passport.authenticate("local")(req, res, function(){
+           res.redirect("/form");
+       });
+    });
+});
+
+//login
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//login route
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/form",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+//logout route
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/");
+});
+
+//Logout
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Server Has Started.");
